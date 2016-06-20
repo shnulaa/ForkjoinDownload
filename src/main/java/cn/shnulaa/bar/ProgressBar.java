a89319c2119083d48de834ede66bafd836f1c88d
shnulaa <ashitawillbefine@gmail.com>
2016-06-20 15:52:35 +0800

    Signed-off-by: shnulaa <ashitawillbefine@gmail.com>

--- src/main/java/cn/shnulaa/bar/ProgressBar.java
+++ src/main/java/cn/shnulaa/bar/ProgressBar.java
@@ -27,7 +27,7 @@ public class ProgressBar {
 
 	public static void main(String[] args) {
 
-		for (int i = 0; i < 100; i++) {
+		for (int i = 0; i <= 100; i++) {
 			System.out.println(showBarByPoint(i, 100, 10, i, true));
 		}
 
